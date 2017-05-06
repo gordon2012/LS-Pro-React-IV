a97cfbdc6ca7e229eaac57875aed65bdd3c97236
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-05-06 09:37:12 -0700

    Add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import styled from 'styled-components';
 
 import About from './components/About';
 import Hobbies from './components/Hobbies';
@@ -7,9 +9,21 @@ import Media from './components/Media';
 
 export default class extends Component {
   render() {
+    const H1 = styled.h1`
+      text-align: center;
+    `;
+    const H2 = styled.h2`
+      text-align: right;
+    `;
+    const Home = () => <Jumbotron>
+      <H1>Hello!</H1>
+      <h2>My name is Gordon.</h2>
+      <H2>I am a Front End Web Developer.</H2>
+    </Jumbotron>
+
     return (
       <Router>
-        <div>
+        <Grid>
           <header>
             <ul>
               <li><Link to="/">Home</Link></li>
@@ -18,11 +32,11 @@ export default class extends Component {
               <li><Link to="/media">Media</Link></li>
             </ul>
           </header>
-          <Route exact path="/" component={() => <h1>Home!</h1>} ></Route>
+          <Route exact path="/" component={Home} ></Route>
           <Route path="/about" component={About}></Route>
           <Route path="/hobbies" component={Hobbies}></Route>
           <Route path="/media" component={Media}></Route>
-        </div>
+        </Grid>
       </Router>
     );
   }
