4d0e47c6d15c4635bd6e5bd7429c79f3082fcdeb
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-05-06 09:11:21 -0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import About from './components/About';
+import Hobbies from './components/Hobbies';
+import Media from './components/Media';
 
 export default class extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div>
+          <header>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/hobbies">Hobbies</Link></li>
+              <li><Link to="/media">Media</Link></li>
+            </ul>
+          </header>
+          <Route exact path="/" component={() => <h1>Home!</h1>} ></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/hobbies" component={Hobbies}></Route>
+          <Route path="/media" component={Media}></Route>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Media.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Media</h2>
+            </div>
+        );
+    }
+}
