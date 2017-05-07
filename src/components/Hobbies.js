c5397650d155cd19c380fd18a28332cebe6661c5
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-05-06 18:27:51 -0700

    Complete Hobbies

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -4,7 +4,38 @@ export default class extends Component {
     render() {
         return (
             <div>
-                <h2>Hobbies</h2>
+                <h2>My Hobbies</h2>
+                <h3>Here are some of my favorite things</h3>
+                <ul>
+                    <li>Video Games
+                        <ul>
+                            <li>EverQuest</li>
+                            <li>Minecraft</li>
+                            <li>Rimworld</li>
+                        </ul>
+                    </li>
+                    <li>Movies
+                        <ul>
+                            <li>The Avengers</li>
+                            <li>Deadpool</li>
+                            <li>Office Space</li>
+                        </ul>
+                    </li>
+                    <li>TV Shows
+                        <ul>
+                            <li>Mr. Robot</li>
+                            <li>Silicon Valley</li>
+                            <li>Game of Thrones</li>
+                        </ul>
+                    </li>
+                    <li>Also enjoy:
+                        <ul>
+                            <li>Gardening</li>
+                            <li>Running</li>
+                            <li>Puppies</li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         );
     }
