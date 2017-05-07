c866bd5e5863a281602ae70d7723adeea9afe5c4
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-05-06 18:27:08 -0700

    Complete About

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ export default class extends Component {
     render() {
         return (
             <div>
-                <h2>About</h2>
+                <h2>About Me</h2>
+                <p>I am a Software Developer at Salt Creek Media Inc. doing Front End Web Development using technologies such as CakePHP, React, Bootstrap, and JQuery.</p>
+                <p>I am learning Full Stack Web Development with a focus on the Front End using Javascript and React. I have recently completed my B.S. degree in Information Technology and am currently attending a self-paced Full Stack Web Development certificate program.</p>
+                <p>The foundation of my technical skills consists of a deep understanding of the fundamental Front-end web technologies including HTML, JavaScript, and CSS.</p>
+                <p>I have used a wide variety of JavaScript libraries and frameworks in my projects including JQuery, React, and Redux. I am well-versed in CSS libraries and preprocessors like Bootstrap and SASS. I have used many build automation tools such as npm, Gulp, and Webpack. I almost exclusively employ version control using Git, GitHub, and the GitHub Flow branching method.</p>
             </div>
         );
     }
